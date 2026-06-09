7aa216d26d765bc5bb5dad26afa369ffe46aa79f
MAcDen34 <d.ngabo@alustudent.com>
2026-06-09 17:31:00 +0200

    feat: redesign UI with a themed Pokédex aesthetic and improve card/details page rendering

--- details.js
+++ details.js
@@ -7,6 +7,18 @@ async function fetchPokemonDetails() {
     const pokemon = await response.json();
 
     console.log(pokemon);
+
+    const container = document.getElementById('pokemon-details');
+    container.innerHTML = `
+        <h1>${pokemon.name}</h1>
+        <img src="${pokemon.sprites.front_default}" alt="${pokemon.name}">
+        <p>ID: ${pokemon.id}</p>
+        <p>Height: ${pokemon.height}</p>
+        <p>Weight: ${pokemon.weight}</p>
+        <p>Type: ${pokemon.types[0].type.name}</p>
+        <p>Abilities: ${pokemon.abilities[0].ability.name}</p>
+        <button onclick="window.location.href='index.html'">Back</button>
+    `;
 }
 
 fetchPokemonDetails();
